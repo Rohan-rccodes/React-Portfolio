ce5b0d4210bb2e84a71f9c4b54078e0c5e578f6f
Rohan Chauhan <140747527+Rohan-rccodes@users.noreply.github.com>
2024-07-03 13:36:14 +0000

    ee

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -21,7 +21,6 @@ import youTubeIcon from "../images/socials/youtube.svg";
 
 /**
  * 💡 Learning resources
- *
  *  HTML hyperlinks: https://www.w3schools.com/html/html_links.asp
  *  Opening links in new tabs: https://www.freecodecamp.org/news/how-to-use-html-to-open-link-in-new-tab/
  */
@@ -61,7 +60,7 @@ const Footer = (props) => {
         }}
       >
         {email && (
-          <a href={`mailto:${email}`}>
+          <a href={`mailto:$itschauhan101@gmail.com`}>
             <img src={envelopeIcon} alt="email" className="socialIcon" />
           </a>
         )}
@@ -71,7 +70,7 @@ const Footer = (props) => {
           </a>
         )}
         {gitHub && (
-          <a href={`https://github.com/${gitHub}`} target="_blank" rel="noopener noreferrer">
+          <a href={`https://github.com/$Rohan-rccodes`} target="_blank" rel="noopener noreferrer">
             <img src={gitHubIcon} alt="GitHub" className="socialIcon" />
           </a>
         )}
